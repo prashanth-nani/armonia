f99c0563d5fcf9aa5ecca705267bc026067c4b83
Prashanth <prashanth1395@gmail.com>
2016-12-29 17:53:45 +0530

    Detecting changes by writing file list to text file

--- listfiles.js
+++ listfiles.js
@@ -1,20 +1,44 @@
 /**
  * Created by prashanth on 17/12/16.
  */
-const fs = require('fs')
+'use strict';
+const fs = require('fs');
 
-function getFiles (dir, files_){
+function getFiles(dir, files_) {
     files_ = files_ || [];
     var files = fs.readdirSync(dir);
-    for (var i in files){
+    for (var i in files) {
         var name = dir + '/' + files[i];
-        if (fs.statSync(name).isDirectory()){
+        if (fs.statSync(name).isDirectory()) {
             getFiles(name, files_);
         } else {
-            files_.push(name);
+            if (name.endsWith(".mp3"))
+                files_.push(name);
         }
     }
     return files_;
 }
 
-console.log(getFiles('/media/prashanth/body/Music'))
+function writeListToFile(list, filePath) {
+    fs.exists(filePath, exits=> {
+        if (exits) {
+            fs.writeFileSync(filePath, list.join("\t"), 'utf8');
+        } else
+            console.log(`${filePath} not found`);
+    });
+}
+
+function readListFromFile(filePath) {
+    if (fs.existsSync(filePath)) {
+        return fs.readFileSync(filePath, 'utf8').split("\t");
+    }
+}
+
+let oldList = readListFromFile("./files.txt");
+let newList = getFiles('/media/prashanth/body/Music');
+
+let removedList = oldList.filter(x => newList.indexOf(x) < 0);
+let addedList = newList.filter(x => oldList.indexOf(x) < 0);
+
+console.log("Deleted " + removedList);
+console.log("Added " + addedList);
