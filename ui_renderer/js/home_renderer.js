cafe6fd7bc85e5c8853164ebd74d845af80435ed
Prashanth <prashanth1395@gmail.com>
2017-01-10 01:54:07 +0530

    Change song ul to table with more info, custom scrollbar, change createList to implement sorting

--- ui_renderer/js/home_renderer.js
+++ ui_renderer/js/home_renderer.js
@@ -6,13 +6,18 @@ var playing = false;
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('armonia.db');
 
-let $songlist = $("#content-list ul");
-db.each("SELECT id AS id, title, artist, genre, location FROM song ORDER BY title", function (err, row) {
-    $songlist.append(`<li data-loc="${row.location}">${row.title}</li>`);
-});
+let $songtable = $('#content-list table >tbody');
+createList("album_name");
+
+function createList(sortElement){
+  $songtable.find('tr:gt(0)').remove();
+  db.each(`SELECT song.id as id, title, artist, location, year, album_id, album_name FROM song, album WHERE song.album_id = album.id ORDER BY ${sortElement}`, function (err, row) {
+      $songtable.append(`<tr data-loc="${row.location}" data-album-id="${row.album_id}"><td class="title">${row.title}</td><td class="artist">${row.artist}</td><td class="album">${row.album_name}</td><td class="year">${row.year}</td></tr>`);
+  });
+}
 
 $(function () {
-    $songlist.on("click", "li", function () {
+    $songtable.on("click", "tr", function () {
         if (playing) {
             player.pause();
             playing = false;
