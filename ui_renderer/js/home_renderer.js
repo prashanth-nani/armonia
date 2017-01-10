3c9b764757c1d3d737ab6d8c78ece3db36a71e3b
Prashanth <prashanth1395@gmail.com>
2017-01-10 16:43:22 +0530

    Add choose music folder option in UI (Functionality not implemented)

--- ui_renderer/js/home_renderer.js
+++ ui_renderer/js/home_renderer.js
@@ -1,23 +1,45 @@
-window.$ = window.jQuery = require("../../src/js/jquery");
+const path = require('path');
+window.$ = window.jQuery = require(path.join(__dirname, "../..", "src/js/jquery"));
 
 var player;
 var playing = false;
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('armonia.db');
+var db = new sqlite3.Database(path.join(__dirname, "..", "..", "armonia.db"));
 
+let $contentlist = $('#content-list');
 let $songtable = $('#content-list table >tbody');
-createList("album_name");
+// let $songtablerows = $('#content-list table >tbody>tr');
+let $choosefolderdiv = $('#select-music-dir');
+let $choosefolderbtn = $('#select-music-dir a');
+
+createList("title");
 
 function createList(sortElement){
   $songtable.find('tr:gt(0)').remove();
   db.each(`SELECT song.id as id, title, artist, location, year, album_id, album_name FROM song, album WHERE song.album_id = album.id ORDER BY ${sortElement}`, function (err, row) {
-      $songtable.append(`<tr data-loc="${row.location}" data-album-id="${row.album_id}"><td class="title">${row.title}</td><td class="artist">${row.artist}</td><td class="album">${row.album_name}</td><td class="year">${row.year}</td></tr>`);
-  });
+      if(err)
+          console.error(err);
+      else{
+        $songtable.append(`<tr data-loc="${row.location}" data-album-id="${row.album_id}"><td class="title">${row.title}</td><td class="artist">${row.artist}</td><td class="album">${row.album_name}</td><td class="year">${row.year}</td></tr>`);
+      }
+  }, showMusic);
+}
+
+function showMusic(){
+  let $songtablerows = $('#content-list table >tbody>tr');
+    if($songtablerows.length>1){
+      $contentlist.show();
+    }else {
+      $choosefolderdiv.show();
+    }
 }
 
 $(function () {
-    $songtable.on("click", "tr", function () {
+    $songtable.on("dblclick", "tr", function () {
+        let $playingrow = $('#content-list table >tbody>tr.selected');
+        $playingrow.removeClass("selected");
+        $(this).addClass("selected");
         if (playing) {
             player.pause();
             playing = false;
