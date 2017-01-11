ff02b21b2ec11caba34d56a6e243b93cae490650
Prashanth <prashanth1395@gmail.com>
2017-01-11 15:43:13 +0530

    Add function to get album art by id

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,18 @@
 const {dialog} = require('electron').remote;
+import path from 'path';
+import glob from 'glob';
 
-export let get_music_dirs = (callback) => {
+export let getMusicDirs = (callback) => {
     "use strict";
     dialog.showOpenDialog({properties: ['openDirectory', 'multiSelections']}, callback);
-}
+};
+
+export let getAlbumArtPathById = (albumArtDir, album_id)=>{
+    "use strict";
+    let pathWithoutExt = path.join(albumArtDir, album_id);
+    glob.glob(`${pathWithoutExt}.*`,function (err, files) {
+        if(err)
+            console.error(err);
+        return files[0];
+    })
+};
