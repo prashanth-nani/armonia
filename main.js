0190269adb2c3be797ca564b6d57440ed34822ac
Prashanth <prashanth1395@gmail.com>
2017-01-17 00:06:01 +0530

    Add minimum window size limit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({minWidth: 800, minHeight: 600, darkTheme: false, title: "Armonia"});
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
